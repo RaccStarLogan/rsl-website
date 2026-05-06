408418a200de8fa346044c6fe1916d9fc8761209
RaccStarLogan <logan-foster@raccstarlogan.com>
2026-05-06 05:00:19 -0500

    people aren't being helpful i nthe slightest

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 import { env } from "cloudflare:workers";
 
-export const post: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request }) => {
   const form = await request.formData();
 
   // Build commission object exactly how the bot expects it
